fbbb7ad3d77861c1051c8e6d0ed6f110a59df021
Marco Juliani <marco@outerlabs.io>
2024-02-24 14:52:06 -0500

    smalle fixes to about page

--- src/About.tsx
+++ src/About.tsx
@@ -19,26 +19,6 @@ export const About: React.FC<{ node: NodeInfo | null }> = ({ node }) => {
         mouseY: 0,
     });
 
-
-    const { mouseX, mouseY } = windowState;
-    const gridRef = useRef<HTMLDivElement | null>(null);
-
-    const distance = useMemo(() => {
-        const gridElement = gridRef.current;
-        if (gridElement) {
-            const rect = gridElement.getBoundingClientRect();
-            const halfWidth = rect.width * 0.5;
-            const halfHeight = rect.height * 0.5;
-            const distanceX = Math.abs(mouseX - rect.left - halfWidth);
-            const distanceY = Math.abs(mouseY - rect.top - halfHeight);
-            const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-            const overallDiagonal = Math.sqrt(windowState.width * windowState.width + windowState.height * windowState.height);
-            const scaledDistance = 50 * ((overallDiagonal / distance));
-            return scaledDistance;
-        }
-        return 200;
-    }, [mouseX, mouseY, gridRef])
-
     useEffect(() => {
         const handleResize = debounce(() => {
             setWindowState(w => {
@@ -51,23 +31,6 @@ export const About: React.FC<{ node: NodeInfo | null }> = ({ node }) => {
             window.removeEventListener('resize', handleResize);
         };
     }, [])
-
-    useEffect(() => {
-        const handleMouseMove = (event: any) => {
-
-            if (Math.abs(event.clientX - windowState.mouseX) < MOVE_BUFFER) return;
-            if (Math.abs(event.clientY - windowState.mouseY) < MOVE_BUFFER) return;
-            setWindowState(s => {
-                return { ...s, mouseX: event.clientX, mouseY: event.clientY }
-            });
-        };
-
-        window.addEventListener('mousemove', handleMouseMove);
-
-        return () => {
-            window.removeEventListener('mousemove', handleMouseMove);
-        };
-    }, []);
 
     if (node == null) return null;
 
@@ -96,17 +59,12 @@ export const About: React.FC<{ node: NodeInfo | null }> = ({ node }) => {
     return (
         <Layout node={node}>
             <Grid container={true} columns={oneCol || twoCols ? 1 : 2} sx={{ width: '100%', height: '100%', paddingBottom: '48px', paddingTop: '24px' }}>
-                <Grid  ref={gridRef} item xs={1} sx={{ width: '100%', display: 'grid', justifyContent: 'center', alignItems: 'center' }}>
+                <Grid  item xs={1} sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                     <Box sx={{
                         borderRadius: '17px',
                         overflow: 'hidden',
                         transition: 'all 0.3s cubic-bezier(.25,.8,.25,1)',
-                        boxShadow: '-2px -1.5px  10px  0px #00000040',
-                        width: (windowState.mouseX) > (windowState.width * 0.5) ? Math.min(distance, width) : 'auto',
-                        height:  (windowState.mouseX) < (windowState.width * 0.5) ? Math.min(distance, width) : 'auto',
-                        display: 'grid',
-                        alignContent: 'center',
-                        justifyContent: 'center',
+                        boxShadow: '-2px -1.5px  10px  0px #00000040'
                     }}>
                         <PortraitImage node={about} width={width} height={width} />
                     </Box>
