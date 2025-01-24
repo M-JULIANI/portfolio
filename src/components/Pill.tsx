229c6cdb585f4e08ca8d01f9ea2db03941f1a514
Marco Juliani <marco@outerlabs.io>
2025-01-23 21:14:15 -0500

    small pill fix

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -1,9 +1,9 @@
 import { FC } from "react";
 
-export const Pill: FC<{ tag: string; key: string }> = ({ tag, key }) => {
+export const Pill: FC<{ tag: string }> = ({ tag }) => {
   return (
     <span
-      key={key}
+      key={tag}
       className="inline-flex items-center justify-center px-2.5 py-0.5 rounded-full text-xs font-['Space_Mono'] text-[#747474] bg-white truncate shadow-sm shadow-black/10"
     >
       {tag}
